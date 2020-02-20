cc60ef7526aa090920e4e0fb97084e55cf2a89df
ERERkin <erkin1399@gmail.com>
2020-02-20 18:56:53 +0600

    Added 56 homework

--- src/main/java/kg/wallet/service/PlusService.java
+++ src/main/java/kg/wallet/service/PlusService.java
@@ -1,6 +1,6 @@
 package kg.wallet.service;
 
-import com.sun.jersey.core.header.MediaTypes;
+import kg.wallet.model.PlusAandB;
 import kg.wallet.model.User;
 
 import javax.ws.rs.GET;
@@ -11,9 +11,8 @@ import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/users")
-public class UserService {
-
+@Path("/plus")
+public class PlusService {
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<User> getAllUsers_Json(){
@@ -21,11 +20,11 @@ public class UserService {
     }
 
     @GET
-    @Path("/{userId}")
+    @Path("/{a}/{b}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public User getUserById(@PathParam("userId") Integer userId){
-        User u = new User();
-        u.setId(userId);
-        return u;
+    public PlusAandB getUserByID(@PathParam("a") Integer a, @PathParam("b") Integer b){
+        PlusAandB plusAandB = new PlusAandB(a + b);
+        System.out.println(plusAandB.getSum());
+        return plusAandB;
     }
 }
